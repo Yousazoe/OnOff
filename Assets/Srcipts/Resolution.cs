fd0148586a341943d5fadcd72584e89fd70195c8
Yousazoe <961652168@qq.com>
2020-12-14 01:35:17 +0800

    1024x768

---
+++ Assets/Srcipts/Resolution.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.SetResolution(1024,768,true);
+    }
+}
